c23a60787b3e401635ab37908cebb96967886649
Tswagerman <thomasswagerman@hotmail.com>
2020-07-16 12:50:00 +0200

    list modified

--- webScraper.py
+++ webScraper.py
@@ -1,24 +1,24 @@
 from bs4 import BeautifulSoup, SoupStrainer
 import requests
 import re
-from weekList import WeekList
+from weekList import List
 
 class WebScraper:
 	def __init__(self):
-		self.weekList = WeekList()
+		self.List = List()
 		WebScraper.main(self)
 	
 	def main(self):
-		weekList = self.weekList.List()
-		print(weekList)
+		List = self.List.createList()
+		print(List)
 		url = "https://www.rentebox.nl/renteoverzicht/renteberichten.aspx?relid=863857FC-96E0-4875-82E3-902151893771"
 		page = requests.get(url)   
 		data = page.text
 		soup = BeautifulSoup(data, "html.parser")
 		#print(soup)
-		print(len(weekList))
-		for i in range(len(weekList)):
-			for link in soup.find_all('a', href = True, text = re.compile(weekList[i])):
+		print(len(List))
+		for i in range(len(List)):
+			for link in soup.find_all('a', href = True, text = re.compile(List[i])):
 				print(link.get('id'))
 				#print(link)
 	
--- weekList.py
+++ weekList.py
@@ -3,26 +3,19 @@ from datetime import date
 import time
 import locale
 
-class WeekList:
+class List:
 	def __init__(self):
-		self.weekList = []
+		self.List = []
 
-	def List(self):
+	def createList(self):
 		locale.setlocale(locale.LC_ALL, 'nl_NL') 
-		today = datetime.datetime.today()
-		todayPlusOne = today + datetime.timedelta(1)
-		todayPlusTwo = today + datetime.timedelta(2)
-		todayPlusThree = today + datetime.timedelta(3)
-		todayPlusFour = today + datetime.timedelta(4)
-		today = datetime.datetime.strftime(today,"%d %B %Y") 
-		todayPlusOne = datetime.datetime.strftime(todayPlusOne,"%d %B %Y") 
-		todayPlusTwo = datetime.datetime.strftime(todayPlusTwo,"%d %B %Y") 
-		todayPlusThree = datetime.datetime.strftime(todayPlusThree,"%d %B %Y") 
-		todayPlusFour = datetime.datetime.strftime(todayPlusFour,"%d %B %Y") 
-		self.weekList = []
-		self.weekList.append(today)
-		self.weekList.append(todayPlusOne)
-		self.weekList.append(todayPlusTwo)
-		self.weekList.append(todayPlusThree)
-		self.weekList.append(todayPlusFour)
-		return self.weekList
+		lenList = 5
+		for i in range(lenList):
+			if i == 0:
+				current = datetime.datetime.today()
+				currentTime = datetime.datetime.strftime(current,"%d %B %Y") 
+			else:
+				current = current + datetime.timedelta(1)
+				currentTime = datetime.datetime.strftime(current,"%d %B %Y") 
+			self.List.append(currentTime)
+		return self.List
